b1247466a5e6c6639088655a6caa2d601de6db43
ziieng <cjengelhardt@gmail.com>
2021-03-02 10:16:18 -0800

    Adjustments for gh-pages publish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import TopNav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
